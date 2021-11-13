edea1aaa37067ed8ec1e7bd2958cb81004d3fee4
John Gallagher <gallagherj0913@gmail.com>
2021-11-13 01:16:08 -0700

    added prompt questions for different functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const sql = require("mysql2");
+const consoleTable = require("console.table");
 
 const openingPrompt = () => {
     return inquirer
@@ -47,18 +48,60 @@ const addRole = () => {
     .prompt([
         {
             type: "input",
-            name: "role",
+            name: "name",
             message: "What is the name of the role?",
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "What is the salary for this role?",
+        },
+        {
+            type: "input",
+            name: "department",
+            message: "Which department does the role belong to?",
         }
     ])
     openingPrompt();
 }
 
 const addEmployee = () => {
+    return inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "first",
+            message: "What is the employee's first name?",
+        },
+        {
+            type: "input",
+            name: "last",
+            message: "What is the employee's last name??",
+        },
+        {
+            type: "input",
+            name: "department",
+            message: "What is the employee's role?",
+        }
+        // Need question: Who is employee's manager? Need a way to have the options come from a managers array?
+    ])
     openingPrompt();
 }
 
 const updateRole = () => {
+    return inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Which employee's role do you want to update?",
+        },
+        {
+            type: "input",
+            name: "role",
+            message: "What role do you want to assign to the selected employee?",
+        }
+    ])
     openingPrompt();
 }
 

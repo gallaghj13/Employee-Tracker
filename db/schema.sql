192ff156065af4f9cde271bfdd95730d864d7440
John Gallagher <gallagherj0913@gmail.com>
2021-11-15 17:54:21 -0700

    fixed typo in schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE company_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30)) NOT NULL
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
